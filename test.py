7f4a883aeef9b035194b85a30de306712510838a
Neha Dwivedi <76174073+dwivedine@users.noreply.github.com>
2021-09-09 06:54:07 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+import subprocess, sys, os
+
+token  = subprocess.check_output('echo $ACTIONS_ID_TOKEN_REQUEST_TOKEN', shell=True)
+#print("token is", token)
+
+url = subprocess.check_output('echo $ACTIONS_ID_TOKEN_REQUEST_URL', shell=True)
+print(url)
